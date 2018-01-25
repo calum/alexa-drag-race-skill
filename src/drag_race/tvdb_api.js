b55e87cb9b9014b39a15dcf6ca883bba5eab2de7
calum <calumforster.play@gmail.com>
2018-01-25 19:03:25 +0000

    fixed lint errors

--- src/drag_race/tvdb_api.js
+++ src/drag_race/tvdb_api.js
@@ -1,4 +1,5 @@
 var request = require('request')
+var winston = require('winston')
 
 if (!process.env.TVDB_API_KEY) {
   winston.error("You must set the environment varaible TVDB_API_KEY to hold your API key for the TVDB. See README for more info.")
@@ -6,8 +7,17 @@ if (!process.env.TVDB_API_KEY) {
 
 var api_key = process.env.TVDB_API_KEY
 
-function getToken() {
+var api_url = 'https://api.thetvdb.com/'
 
+function getToken(callback) {
+  request.post(api_url+'login', {"apikey":api_key}, function(err, res) {
+    if(err) {
+      winston.error(err)
+      return callback(err)
+    }
+    var body = JSON.stringify(res).body
+    return callback(null, body.token)
+  })
 }
 
 function getNextEpisode() {
